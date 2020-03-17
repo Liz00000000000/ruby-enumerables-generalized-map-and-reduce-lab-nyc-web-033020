5d6fad36c15ab45f4da0cdbaffd28380b211e551
Elizabeth Karen <elizabeth.michael.karen@gmail.com>
2020-03-17 15:22:16 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -11,5 +11,16 @@ end
 
 
 def reduce(source_array, starting_value = nil)
+ if starting_value
+  num1 = starting
+  i = 0
+else
+   num1 = [0]
+    i = 1
+  end
+
+  while i < source_array.size do
+    yield(num1, array[i]) 
+  end
 
 end
